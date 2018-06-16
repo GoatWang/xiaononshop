3d5e28910eea3c6d3da4728b249ad79edc695125
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-06-16 10:31:20 +0800

    testline

--- order/views.py
+++ order/views.py
@@ -25,10 +25,14 @@ def index(request):
 ## handle message and event
 def _handle_text_msg(event):
     text = event.message.text
-    user_id = event.source.user_id
+    print(text)
+    print(text)
+    print(text)
+    print(text)
+    # user_id = event.source.user_id
     # user_name = line_bot_api.get_profile(user_id).display_name
     # messages = [TextSendMessage(text=user_name+": "+text)]
-    messages = [TextSendMessage(text=text)]
+    messages = TextSendMessage(text="test")
     line_bot_api.reply_message(
         event.reply_token,
         messages

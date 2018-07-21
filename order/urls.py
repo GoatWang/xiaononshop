5ad0af490be6951d089306890be2e12590d0a8f6
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-07-21 11:12:54 +0800

    test login

--- order/urls.py
+++ order/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
     path('order_create/', views.order_create, name='order_create'),
-    # path('order_create/<str:line_id>/', views.order_create, name='order_create'),
     path('order_create/<int:area_id>/<int:distribution_place_id>/', views.order_create, name='order_create'),
-    path('order_list/<str:line_id>/', views.order_list, name='order_list'),
-    path('line_login_callback/', views.line_login_callback, name='line_login_callback'),
+
+    path('order_list/', views.order_list, name='order_list'),
+
     path('line_login_callback/<str:app_name>/<str:view_name>', views.line_login_callback, name='line_login_callback'),
+
     path('callback/', views.callback, name='callback'),
 ]

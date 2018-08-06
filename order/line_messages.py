26c9368e072cc9ed08937a4314195743df699719
jeremy4555 <jeremy4555@yahoo.com.tw>
2018-08-06 21:48:48 +0800

    add weekly bento

--- order/line_messages.py
+++ order/line_messages.py
@@ -105,7 +105,7 @@ def get_order_list_reply(user):
         carousel_columns = []
         for order in current_orders:
             carousel_column = CarouselColumn(
-                        thumbnail_image_url='https://s3-ap-southeast-1.amazonaws.com/ogbento/' + str(order['photo']),
+                        thumbnail_image_url=settings.AWS_BENTO_IMG_URL + str(order['photo']),
                         title=order['date'] + ' ' + order['name'],
                         text="個數: " + str(order['number']) + "個\n取餐地點: " + order['distribution_place'],
                         actions=[
@@ -146,53 +146,45 @@ def get_weekly_bentos_reply():
         messages = [TextSendMessage(text="目前沒有便當供應，請開學後再來找我喔。")]
         return messages
     else:
-        aws_url = settings.AWS_BENTO_IMG_URL
         df_available_bentos = pd.DataFrame(list(available_bentos))
         df_available_bentos['id'] = df_available_bentos['bento__id']
         df_available_bentos['name'] = df_available_bentos['bento__name']
         df_available_bentos['date'] = df_available_bentos['bento__date'].apply(lambda x:str(x.month) + '/' + str(x.day))
         df_available_bentos['bento_type'] = df_available_bentos['bento__bento_type__bento_type']
         df_available_bentos['cuisine'] = df_available_bentos['bento__cuisine']
-        df_available_bentos['photo'] = df_available_bentos['bento__photo'].apply(lambda x:aws_url + x)
+        df_available_bentos['photo'] = df_available_bentos['bento__photo']
         df_available_bentos['price'] = df_available_bentos['bento__price']
         df_available_bentos['remain'] = df_available_bentos['total_remain'].astype(str)
 
         df_available_bentos = df_available_bentos[[ "id", "name", "date", "bento_type", "cuisine", "photo", "price", "remain"]]
         available_bentos = df_available_bentos.T.to_dict().values()
 
-        carousel_columns = []
-        for bento in available_bentos:
-            carousel_column = CarouselColumn(
-                        thumbnail_image_url=settings.AWS_BENTO_IMG_URL + str(bento['photo']),
-                        title=bento['date'] + ' ' + bento['name'],
-                        text=bento['bento_type'] + ": " + bento['cuisine'] + "\n" +  str(bento['price'])  + "元/個(剩" + str(bento['remain']) + "個)",
-                        actions=[
-                            MessageTemplateAction(
-                                label='馬上訂購',
-                                text='動作: 馬上訂購'
-                            )
-                        ]
-                    )
-            carousel_columns.append(carousel_column)
 
-        carousel_template_message = TemplateSendMessage(
-            alt_text='本週菜單',
-            template=CarouselTemplate(
-                columns=carousel_columns
-            )
-        )
+        messages = []
+        for i in range(2):
+            carousel_columns = []
+            for bento in available_bentos[i:(i+1)*5]:
+                carousel_column = CarouselColumn(
+                            thumbnail_image_url=settings.AWS_BENTO_IMG_URL + str(bento['photo']),
+                            title=bento['date'] + ' ' + bento['name'] + "(剩" + str(bento['remain']) + "個)",
+                            text=bento['bento_type'] + ": " + bento['cuisine'] + "\n" +  str(bento['price'])  + "元/個",
+                            actions=[
+                                MessageTemplateAction(
+                                    label='馬上訂購',
+                                    text='動作: 馬上訂購'
+                                )
+                            ]
+                        )
+                carousel_columns.append(carousel_column)
 
-        buttons_template_message = TemplateSendMessage(
-            alt_text='查看訂單提醒',
-            template=ButtonsTemplate(
-                text='本頁面僅提供五筆訂單資訊，查看完整訂單資訊，請點擊下面按鈕。',
-                actions=[
-                    URITemplateAction(
-                        label='完整訂單資訊',
-                        uri=settings.DOMAIN + 'order/order_list/'
-                    )
-                ]
+            carousel_template_message = TemplateSendMessage(
+                alt_text='本週菜單',
+                template=CarouselTemplate(
+                    columns=carousel_columns
+                )
             )
-        )
-        return [carousel_template_message]
+            messages.append(carousel_template_message)
+
+        messages.append(TextSendMessage(text=settings.AWS_BENTO_IMG_URL + str(bento['photo'])))
+        return messages
 
